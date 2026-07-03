e4436141645698cfc18d872e620b00585e3d80fb
cala-jhonni1046 <calajhonni@gmail.com>
2026-07-03 13:01:55 +0000

    Estructura base del proyecto: listo para colaboracion

---
+++ src/main/java/com/gimnasio/gestion_gimnasio/GestionGimnasioApplication.java
@@ -0,0 +1,13 @@
+package com.gimnasio.gestion_gimnasio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionGimnasioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionGimnasioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gimnasio/gestion_gimnasio/GestionGimnasioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimnasio.gestion_gimnasio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionGimnasioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
